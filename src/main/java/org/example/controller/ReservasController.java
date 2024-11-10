dd71c84909a2f1f2806b2ba9dd8ede07b11b3715
Sebarambi <sebarambi@yahoo.com>
2024-11-10 16:25:12 -0300

    rua rua

--- src/main/java/org/example/controller/ReservasController.java
+++ src/main/java/org/example/controller/ReservasController.java
@@ -1,6 +1,8 @@
 package org.example.controller;
 
 import org.example.DAO.ReservasDAO;
+import org.example.model.Hotel;
+import org.example.model.Reservas;
 
 public class ReservasController {
     private ReservasDAO reservasDAO = new ReservasDAO();
@@ -8,5 +10,7 @@ public class ReservasController {
     public ReservasController() {
         this.reservasDAO = new ReservasDAO();
     }
-
+    public boolean insertarReserva(Reservas reservas) {
+        return this.reservasDAO.insertReserva(reservas);
+    }
 }
